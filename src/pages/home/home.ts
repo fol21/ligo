4f67a5ae83d7cf21485cdfbd502e7e100a3c610e
felipe <felipegs31@gmail.com>
2018-01-19 14:16:40 -0200

    tela home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,22 +14,26 @@ export class HomePage {
       nome: 'Oi',
       sinal: 50,
       preco3g: 10,
-      selected: true
+      selected: true,
+      icone: "oi-icone"
     }, {
       nome: 'Tim',
       sinal: 30,
       preco3g: 12,
-      selected: false
+      selected: false,
+      icone: "tim-icone"
     }, {
       nome: 'Claro',
       sinal: 40,
       preco3g: 9,
-      selected: false
+      selected: false,
+      icone: "claro-icone"
     }, {
       nome: 'Vivo',
       sinal: 60,
       preco3g: 11,
-      selected: false
+      selected: false,
+      icone: "vivo-icone"
     }];
 
 
